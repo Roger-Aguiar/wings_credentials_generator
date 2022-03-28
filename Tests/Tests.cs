0e1b4ff3624260d3432b25bc8304efef4530d1ad
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-03-28 10:17:43 -0300

    It refactors the code and a test

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -7,7 +7,8 @@ namespace WingsCredentialsApproval
         [Fact]
         public void GenerateAccessTokenTest()
         {
-            var authentication = new Authentication("c3b9d872-d137-3f79-b801-8ac182f8379f", "e57c37fc-236b-3204-9e54-aa25abde9111");
+            Headers headers = new Headers("c3b9d872-d137-3f79-b801-8ac182f8379f", "e57c37fc-236b-3204-9e54-aa25abde9111");
+            var authentication = new Authentication(headers);
             
             Assert.Equal(36, authentication.GenerateAccessToken().Length);
             Assert.NotEmpty(authentication.GenerateAccessToken());
